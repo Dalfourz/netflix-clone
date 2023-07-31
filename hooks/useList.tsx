49a3f594c9f817e481dda28b4a68d89e863fda19
Dalfour <yjulius.mail@gmail.com>
2023-08-01 08:57:12 +1000

    restage

--- hooks/useList.tsx
+++ hooks/useList.tsx
@@ -4,25 +4,25 @@ import { DocumentData, collection, onSnapshot } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 function useList(uid: string | undefined) {
-    const [list, setList] = useState<Movie[] | DocumentData[]>([])
-    
-    useEffect(() => {
-        if (!uid) return
-    
-        return onSnapshot(
-          collection(db, 'customers', uid, 'myList'),
-          (snapshot) => {
-            setList(
-              snapshot.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data(),
-              }))
-            )
-          }
-        )
-      }, [db, uid])
+  const [list, setList] = useState<Movie[] | DocumentData[]>([]);
 
-    return list;
+  useEffect(() => {
+    if (!uid) return;
+
+    return onSnapshot(
+      collection(db, "customers", uid, "myList"),
+      (snapshot) => {
+        setList(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+          }))
+        );
+      }
+    );
+  }, [db, uid]);
+
+  return list;
 }
 
 export default useList;
